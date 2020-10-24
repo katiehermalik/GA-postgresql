58509cd42ad4d9ce5180cc954363e4afc964355c
khermalik <khermalik@gmail.com>
2020-10-23 20:04:45 -0700

    solved clue #4

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,36 +2,26 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-SELECT * 
-FROM country 
-WHERE population = (SELECT MIN(population) FROM country WHERE region = 'Southern Europe');
+Holy See (Vatican City State)
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
-SELECT language
-FROM countrylanguage
-WHERE countrycode = 'VAT'
-AND isofficial = TRUE;
+Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
-SELECT name
-FROM countrylanguage
-INNER JOIN country ON countrycode = code
-WHERE language = 'Italian'
-AND percentage = 100;
-
+San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
