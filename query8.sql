d223f9e806547674c4f2e528f8d5535a7fdef6b5
khermalik <khermalik@gmail.com>
2020-10-24 19:09:21 -0700

    solved Clue #8

---
+++ query8.sql
@@ -0,0 +1,15 @@
+-- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- if we can crack it, we can finally put her where she belongs – behind bars.
+
+-- Our play date of late has been unusually fun –
+-- As an agent, I'll say, you've been a joy to outrun.
+-- And while the food here is great, and the people – so nice!
+-- I need a little more sunshine with my slice of life.
+-- So I'm off to add one to the population I find
+-- In a city of ninety-one thousand and now, eighty five.
+
+SELECT name
+FROM city
+WHERE population = 91084;
+
+-- Santa Monica
